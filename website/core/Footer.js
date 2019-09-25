07d310d502aec5a0694ee0501da86665316b8591
Damian ONeill <damian.oneill@me.com>
2019-09-25 13:01:36 +0100

    remove content from footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -25,7 +25,7 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
+          {/* <a href={this.props.config.baseUrl} className="nav-home">
             {this.props.config.footerIcon && (
               <img
                 src={this.props.config.baseUrl + this.props.config.footerIcon}
@@ -34,13 +34,7 @@ class Footer extends React.Component {
                 height="58"
               />
             )}
-          </a>
-          <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl("quickstart.html", this.props.language)}>
-              Quickstart
-            </a>
-          </div>
+          </a> */}
         </section>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
